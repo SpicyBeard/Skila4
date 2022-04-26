47fefd9e6e21d246b1e10dc95ecd9855b4dc0d34
SpicyBeard <103431080+SpicyBeard@users.noreply.github.com>
2022-04-26 11:33:59 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 const bodyParser = require('body-parser');
 const realPassword = 'Ligma';
-
+const mongo = require('mongodb').MongoClient;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -26,42 +26,94 @@ app.get('/', (req, res) => {
   // með næstu skipun getum við séð í console hvenær beiðni barst 
   console.log('Site visitor arrived at: '+req.requestTime); 
 }); 
-  
-io.on('connection', (socket) => {
-  //Hækka fjölda innskráðra
-  connectedUsers++;
-  //Látum aðra vita að fjöldi breyttist.
-  io.emit('Connected users changed', connectedUsers);
-  console.log('user connected');
-  socket.on('disconnect', () => {
-    //Lækka fjölda innskráðra
-    connectedUsers--;
+ 
+//skila skila4cluster.r12qr.mongodb.net
+mongo.connect('mongodb://127.0.0.1/chatserver_msg',{useUnifiedTopology: true}, function(err,db){
+  if (err){
+    throw err;
+  }
+  var chatdb = db.db('chatserver_msg');
+
+  io.on('connection', (socket) => {
+    //Hækka fjölda innskráðra
+    connectedUsers++;
     //Látum aðra vita að fjöldi breyttist.
     io.emit('Connected users changed', connectedUsers);
-    console.log('user disconnected');
-  });
-  socket.on('choose_nick', (username) => {
-    console.log('username: ' + username.username);
-    if (username.password === realPassword)
-      socket.userName = username.username;
-  });
-  socket.on('chat message', (msg) => {
-      // console.log(msg.password);
-      console.log(socket.userName + ' said: ' + msg.value);
-      if (msg.password === realPassword)
-        io.emit('chat message', msg, socket.userName);
-  });
-  socket.on('typing', (data)=>{
-    data.userName = socket.userName;
-    // console.log(data);
-    if(data.typing==true) {
-      if (data.password === realPassword)
+    console.log('user connected');
+    socket.on('disconnect', () => {
+      //Lækka fjölda innskráðra
+      connectedUsers--;
+      //Látum aðra vita að fjöldi breyttist.
+      io.emit('Connected users changed', connectedUsers);
+      console.log('user disconnected');
+    });
+    chatdb.collection('messages').find({}).toArray(function(err, result){
+      if (err) throw err;
+      console.log(result);
+      socket.emit('initialize_chat',result);
+    });
+    socket.on('choose_nick', (username) => {
+      console.log('username: ' + username.username);
+      if (username.password === realPassword)
+        socket.userName = username.username;
+    });
+    socket.on('chat_message', (msg) => {
+        // console.log(msg.password);
+
+        console.log(socket.userName + ' said: ' + msg.value);
+        if (msg.password === realPassword)
+          chatdb.collection('messages').insertOne({user: socket.userName,msg:msg});
+          //io.emit('chatmsg',msg);
+          io.emit('chat_message', msg, socket.userName);
+    });
+    socket.on('typing', (data)=>{
+      data.userName = socket.userName;
+      // console.log(data);
+      if(data.typing==true) {
+        if (data.password === realPassword)
+          io.emit('display', data)
+      }
+      else
         io.emit('display', data)
-    }
-    else
-      io.emit('display', data)
-  })
+    });
+  });
 });
+
+// io.on('connection', (socket) => {
+//   //Hækka fjölda innskráðra
+//   connectedUsers++;
+//   //Látum aðra vita að fjöldi breyttist.
+//   io.emit('Connected users changed', connectedUsers);
+//   console.log('user connected');
+//   socket.on('disconnect', () => {
+//     //Lækka fjölda innskráðra
+//     connectedUsers--;
+//     //Látum aðra vita að fjöldi breyttist.
+//     io.emit('Connected users changed', connectedUsers);
+//     console.log('user disconnected');
+//   });
+//   socket.on('choose_nick', (username) => {
+//     console.log('username: ' + username.username);
+//     if (username.password === realPassword)
+//       socket.userName = username.username;
+//   });
+//   socket.on('chat message', (msg) => {
+//       // console.log(msg.password);
+//       console.log(socket.userName + ' said: ' + msg.value);
+//       if (msg.password === realPassword)
+//         io.emit('chat message', msg, socket.userName);
+//   });
+//   socket.on('typing', (data)=>{
+//     data.userName = socket.userName;
+//     // console.log(data);
+//     if(data.typing==true) {
+//       if (data.password === realPassword)
+//         io.emit('display', data)
+//     }
+//     else
+//       io.emit('display', data)
+//   })
+// });
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
